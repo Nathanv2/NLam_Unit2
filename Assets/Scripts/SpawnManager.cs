588afb34588d70021fd591a7e626cb571248cf9f
Nathanv2 <lnathan718@gmail.com>
2023-09-21 18:30:44 -0700

    Finished Unit 2 Lesson C

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -20,7 +20,12 @@ public class SpawnManager : MonoBehaviour
             float randXPos = Random.Range(-xPosRange, xPosRange);
             int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
             Vector3 randPos = new Vector3(randXPos, 0, 21);
-            Instantiate(animalPrefabs[animalPrefabIndex], new Vector3(randXPos, 0, 21), animalPrefabs[animalPrefabIndex].transform.rotation);
+            Instantiate(animalPrefabs[animalPrefabIndex], randPos, animalPrefabs[animalPrefabIndex].transform.rotation);
         }
+    }
+
+    void SpawnRandomwAnimal()
+    {
+
     }
 }
